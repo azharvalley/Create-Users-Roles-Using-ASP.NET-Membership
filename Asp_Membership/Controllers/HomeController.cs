f812cf9796262274d7c4f8c276339b81790cee57
Md Kamal Azhar <kamalazhar571@gmail.com>
2018-07-25 18:06:39 +0530

    Add files via upload

---
+++ Asp_Membership/Controllers/HomeController.cs
@@ -0,0 +1,225 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+
+namespace Asp_Membership.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult CreateRole()
+        {
+
+            string[] AllRoles =  Roles.GetAllRoles();
+
+            ViewBag.AllRoles = AllRoles;
+
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult CreateRole(string a)
+        {
+            
+            string roleName = Request.Params["txtRole"];
+            if(!Roles.RoleExists(roleName))
+            {
+                Roles.CreateRole(roleName);
+                ViewBag.Success = "Role " + "\"" + roleName + "\"" + " successfully created";
+            }
+            else
+            {
+                ViewBag.ErrorMessage = "Role " + "\"" +  roleName + "\"" + " already exists";
+            }
+            string[] AllRoles = Roles.GetAllRoles();
+            ViewBag.AllRoles = AllRoles;
+            return View();
+        }
+
+
+        public ActionResult CreateUser()
+        {
+            string[] AllRoles = Roles.GetAllRoles();
+            ViewBag.AllRoles = AllRoles;
+
+            MembershipUserCollection AllUsers = Membership.GetAllUsers();
+            ViewBag.AllUsers = AllUsers;
+
+            string UserName = Request.Params["email"];
+
+            if (Request.Params["D"] !=null)
+            {
+                if(Request.Params["D"] == "1")
+                {
+                    ViewBag.Success = "User " + "\"" + UserName + "\"" + " removed successfully!";
+                }
+                else if(Request.Params["D"] == "1")
+                {
+                    ViewBag.ErrorMessage = "User " + "\"" + UserName + "\"" + " does not exists!";
+                }
+            }
+
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult CreateUser(string a)
+        {
+            string Email = Request.Params["txtEmail"];
+            string Password = Request.Params["txtPassword"];
+            string SecurityQuestion = Request.Params["txtSecurityQuestion"];
+            string SecurityAnswer = Request.Params["txtSecurityAnswer"];
+            string Role = Request.Params["drpRole"];
+
+                       
+
+            if(!Roles.RoleExists(Role))
+            {
+                ViewBag.ErrorMessage = "Role " + "\"" + Role + "\"" + " does not exists!";
+            }
+            else
+            {
+                MembershipUser user = Membership.GetUser(Email);
+                if (user == null)
+                {
+                    MembershipCreateStatus ss;
+                    Membership.CreateUser(Email, Password, Email, SecurityQuestion, SecurityAnswer, true, out ss);
+                    if (ss == MembershipCreateStatus.Success)
+                    {
+                        user = Membership.GetUser(Email);
+                        if (user != null)
+                        {
+                            Roles.AddUserToRole(Email, Role);
+                            ViewBag.Success = "User " + "\"" + Email + "\"" + " successfully created!";
+                        }
+                    }
+                    else
+                    {
+                        ViewBag.ErrorMessage = "Some error occurred. Please try again!";
+                    }
+                }
+                else
+                {
+                    ViewBag.ErrorMessage = "User " + "\"" + Email + "\"" + " already exists!";
+                }
+            }
+
+            
+
+            string[] AllRoles = Roles.GetAllRoles();
+            ViewBag.AllRoles = AllRoles;
+
+            MembershipUserCollection AllUsers = Membership.GetAllUsers();           
+            ViewBag.AllUsers = AllUsers;
+
+            return View();
+        }
+
+        
+
+        [HttpGet]
+        public ActionResult RemoveUser(string email)
+        {
+            string UserName = email;
+
+            MembershipUser user = Membership.GetUser(UserName);
+
+            if(user!=null)
+            {
+                Membership.DeleteUser(UserName, true);
+                Roles.DeleteRole(UserName);
+
+                ViewBag.Success = "User " + "\"" + UserName + "\"" + " removed successfully!";
+
+                Response.Redirect("/Home/CreateUser?email="+UserName+"&D=1");
+            }
+            else
+            {
+                ViewBag.ErrorMessage = "User " + "\"" + UserName + "\"" + " does not exists!";
+                Response.Redirect("/Home/CreateUser?email=" + UserName + "&D=2");
+            }
+
+
+            
+
+            return View();
+        }
+
+
+        [AllowAnonymous]
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [AllowAnonymous]
+        [ValidateAntiForgeryToken]
+        public ActionResult Login(string returnUrl)
+        {
+            string Email = Request.Params["txtEmail"];
+            string Password = Request.Params["txtPassword"];
+
+            if (!String.IsNullOrEmpty(Email) && !String.IsNullOrEmpty(Password))
+            {
+                string Role = "";
+                string[] UserRole = Roles.GetRolesForUser(Email);
+                if (UserRole.Length > 0)
+                {
+                    Role = UserRole[0].ToString();
+                }
+
+                if (Membership.ValidateUser(Email, Password))
+                {
+                    FormsAuthentication.SetAuthCookie(Email, true);
+                    Response.Redirect("/Home/Dashobard");
+                }
+                else
+                {
+                    ViewBag.ErrorMessage = "User " + "\"" + Email + "\"" + " does not exists!";
+                }
+
+
+            
+
+
+            }
+
+            return View();
+        }
+
+        [Authorize]
+        public ActionResult Dashobard()
+        {
+            string Email = "";
+            if (User.Identity.IsAuthenticated)
+            {
+                Email = User.Identity.Name;
+            }
+
+            ViewBag.Email = Email;
+
+            string[] roles =  Roles.GetRolesForUser(Email);
+            ViewBag.Roles = roles;
+
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult LogOff()
+        {
+            Session.Abandon();
+            FormsAuthentication.SignOut();
+            return RedirectToAction("Login", "Home");
+        }
+    }
+}
---
+++ Asp_Membership/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Asp_Membership.MvcApplication" Language="C#" %>
