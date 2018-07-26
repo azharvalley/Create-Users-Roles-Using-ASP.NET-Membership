d7fd4a9122e67c8bf270af6b8029fed9c9ce8f9a
Md Kamal Azhar <kamalazhar571@gmail.com>
2018-07-26 16:43:46 +0530

    My Model Class And Database connection

---
+++ Asp_Membership/Models/DBConnect.cs
@@ -0,0 +1,42 @@
+﻿using SubSonic.Schema;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+
+namespace Asp_Membership.Models
+{
+    public class DBConnect
+    {
+        public static DataTable HasLocalAccount(string UserId)
+        {
+            SubSonic.Schema.StoredProcedure sp = new StoredProcedure("HasLocalAccount");
+
+            sp.Command.AddParameter("@UserId", UserId, DbType.String);
+
+            return sp.ExecuteDataSet().Tables[0];
+        }
+
+        public static string Changepassword(string UserId, string Password)
+        {
+            SubSonic.Schema.StoredProcedure sp = new StoredProcedure("changepassword");
+
+            sp.Command.AddParameter("@userId", UserId, DbType.String);
+            sp.Command.AddParameter("@password", Password, DbType.String);
+
+            return sp.ExecuteScalar<String>();
+
+        }
+
+        static public DataTable selectUserId(string email)
+        {
+            SubSonic.Schema.StoredProcedure sp = new StoredProcedure("aspnet_Membership_GetUserId");
+
+            sp.Command.AddParameter("@Username", email, DbType.String);
+
+            return sp.ExecuteDataSet().Tables[0];
+
+        }
+    }
+}
---
+++ Asp_Membership/Models/MyModel.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Asp_Membership.Models
+{
+    public class MyModel
+    {
+        public enum ManageMessageId
+        {
+            ChangePasswordSuccess,
+            SetPasswordSuccess,
+            RemoveLoginSuccess,
+        }
+        public class LocalPasswordModel
+        {
+            [Required]
+            [DataType(DataType.Password)]
+            [Display(Name = "Old password")]
+            public string OldPassword { get; set; }
+
+            [Required]
+            [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
+            [DataType(DataType.Password)]
+            [Display(Name = "New password")]
+            public string NewPassword { get; set; }
+
+            [DataType(DataType.Password)]
+            [Display(Name = "Confirm new password")]
+            [Compare("NewPassword", ErrorMessage = "The new password and confirmation password do not match.")]
+            public string ConfirmPassword { get; set; }
+        }
+    }
+}
